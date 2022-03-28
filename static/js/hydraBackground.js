1682379496ccb9b9dabf6bcf714589beb536c4c3
fmoraitis <fmoraitis.gmail.com>
2022-03-28 22:43:34 +0300

    hydra5

--- static/js/hydraBackground.js
+++ static/js/hydraBackground.js
@@ -3,15 +3,25 @@ var hydra = new Hydra({
     canvas: document.getElementById("myCanvas2"),
     detectAudio: false
 })
-osc(10, 0.1, 0.8).rotate(0, 0.1).kaleid().color(-1, 1)
-    .scale(1, () => window.innerHeight / window.innerWidth)
-    .out()
+
 
 osc(1, 0.2, 10)
     .repeat(10, 10)
-    osc(10, 0.1, 0.8).rotate(0, 0.1).color(-0.5, 1)
+osc(10, 0.1, 0.8).rotate(0, 0.1).color(-0.5, 1)
     .modulate(o0, 0.2)
-    .brightness(10).contrast(0.2).saturate(20)
+    // .brightness(10).contrast(0.2).saturate(20)
+
+
+voronoi(2, 0.3, 0.2).shift(0.5)
+    .modulatePixelate(voronoi(4, 0.2), 32, 2)
+
+    .diff(voronoi(3).shift(0.6))
+    .diff(osc(2, 0.15, 1.1).rotate())
+    .brightness(2.3).contrast(0.2).saturate(0.9)
+
     .scale(1, () => window.innerHeight / window.innerWidth)
-    .out(o0)
+    .out()
+
+    
+
 // create functions to use with buttons
