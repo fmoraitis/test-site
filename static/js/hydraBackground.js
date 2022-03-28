ce94afe0aec759758b119a50d1d7259d3e8d12bf
fmoraitis <fmoraitis.gmail.com>
2022-03-28 23:07:30 +0300

    hydra6

--- static/js/hydraBackground.js
+++ static/js/hydraBackground.js
@@ -5,19 +5,19 @@ var hydra = new Hydra({
 })
 
 
-osc(1, 0.2, 10)
+osc(0.1, 0.2, 10)
     .repeat(10, 10)
-osc(10, 0.1, 0.8).rotate(0, 0.1).color(-0.5, 1)
+
     .modulate(o0, 0.2)
     // .brightness(10).contrast(0.2).saturate(20)
 
 
-voronoi(2, 0.3, 0.2).shift(0.5)
-    .modulatePixelate(voronoi(4, 0.2), 32, 2)
+
+   
 
     .diff(voronoi(3).shift(0.6))
-    .diff(osc(2, 0.15, 1.1).rotate())
-    .brightness(2.3).contrast(0.2).saturate(0.9)
+    .diff(osc(2, 0.015, 1.1).rotate())
+    .brightness(2.2).contrast(0.2).saturate(0.9)
 
     .scale(1, () => window.innerHeight / window.innerWidth)
     .out()
