66a6501d4c15d096443000efe6239d64a14eb0e2
fmoraitis <fmoraitis.gmail.com>
2022-03-28 20:06:22 +0300

    hydra

---
+++ static/js/hydraBackground.js
@@ -0,0 +1,17 @@
+// create a new hydra-synth instance
+var hydra = new Hydra({
+    canvas: document.getElementById("myCanvas2"),
+    detectAudio: false
+})
+osc(10, 0.1, 0.8).rotate(0, 0.1).kaleid().color(-1, 1)
+    .scale(1, () => window.innerHeight / window.innerWidth)
+    .out()
+
+osc(1, 0.2, 10)
+    .repeat(10, 10)
+    osc(10, 0.1, 0.8).rotate(0, 0.1).color(-1, 1)
+    .modulate(o0, 0.2)
+    .brightness(3).contrast(0.2).saturate(5)
+    .scale(1, () => window.innerHeight / window.innerWidth)
+    .out(o0)
+// create functions to use with buttons
