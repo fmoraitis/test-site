39123eb6f13fe38060caffa43bcc72a59e483176
fmoraitis <fmoraitis.gmail.com>
2022-03-29 11:57:53 +0300

    hydra 7 fixed

--- static/js/hydraBackground.js
+++ static/js/hydraBackground.js
@@ -5,23 +5,16 @@ var hydra = new Hydra({
 })
 
 
-osc(0.1, 0.2, 10)
-    .repeat(10, 10)
-
-    .modulate(o0, 0.2)
+    osc(0.1, 0.2, 10)
+    .modulate(o0, 0.20)
     // .brightness(10).contrast(0.2).saturate(20)
-
-
-
-   
-
-    .diff(voronoi(3).shift(0.6))
+    .diff(voronoi(30).shift(0.6))
     .diff(osc(2, 0.015, 1.1).rotate())
     // .brightness(2.2).contrast(0.2).saturate(0.9)
-    .brightness(2.2).contrast(0.2).saturate(2)
+    .brightness(3).contrast(0.2).saturate(5)
     .scale(1, () => window.innerHeight / window.innerWidth)
     .out()
 
-    
+
 
 // create functions to use with buttons
