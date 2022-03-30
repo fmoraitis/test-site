12e1e0f265c0c25f7cf3638e9c9dc2bb8ff084db
fmoraitis <fmoraitis.gmail.com>
2022-03-31 00:59:18 +0300

    glsl interact mouse

--- static/js/hydraBackground.js
+++ static/js/hydraBackground.js
@@ -4,7 +4,23 @@ var hydra = new Hydra({
     detectAudio: false
 })
 
+bg01()
 
+function bg02(){
+    osc(20, 0.01, 1.1)
+	.kaleid(5)
+	.color(2.83,0.91,0.39)
+	.rotate(0, 0.1)
+	.modulate(o0, () => mouse.x *mouse.y* 0.003)
+	.scale(1.01)
+  	
+   
+    .scale(1, () => window.innerHeight / window.innerWidth)
+
+    .out(o0)
+
+}
+function bg01(){
     osc(10, 0.2, 10)
     .modulate(o0, 0.20)
     // .brightness(10).contrast(0.2).saturate(20)
@@ -12,9 +28,9 @@ var hydra = new Hydra({
     .diff(osc(2, 0.015, 1.1).rotate())
     // .brightness(2.2).contrast(0.2).saturate(0.9)
     .brightness(20).contrast(0.02).saturate(10)
+    .modulate(o0, () => mouse.x * 0.0003)
     .scale(1, () => window.innerHeight / window.innerWidth)
-    .out()
-
-
+    .out(o0)
+}
 
 // create functions to use with buttons
