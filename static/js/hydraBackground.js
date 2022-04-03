d437a662b56a21c5fac6d60423a089b4a0e9a84b
fmoraitis <fmoraitis.gmail.com>
2022-04-03 21:19:47 +0300

    font size

--- static/js/hydraBackground.js
+++ static/js/hydraBackground.js
@@ -7,17 +7,42 @@ var hydra = new Hydra({
 bg01()
 
 function bg02(){
-    osc(20, 0.01, 1.1)
-	.kaleid(5)
-	.color(2.83,0.91,0.39)
-	.rotate(0, 0.1)
-	.modulate(o0, () => mouse.x *mouse.y* 0.003)
-	.scale(1.01)
-  	
-   
-    .scale(1, () => window.innerHeight / window.innerWidth)
+    osc( 215, 0.1, 2 )
+.modulate(
+  osc( 2, -0.3, 100 )
+  .rotate(15)
+)
+.mult(
+  osc( 215, -0.1, 2)
+  .pixelate( 50, 50 )
+)
+.color( 0.9, 0.0, 0.9 )
+.modulate(
+  osc( 6, -0.1 )
+  .rotate( 9 )
+)
+.add(
+  osc( 10, -0.9, 900 )
+  .color(1,0,1)
+)
+.mult(
+  shape(900, 0.2, 1)
+  .luma()
+  .repeatX(2)
+  .repeatY(2)
+  .colorama(10)
+)
+.modulate(
+  osc( 9, -0.3, 900 )
+  .rotate( 6 )
+)
+.add(
+  osc(4, 1, 90)
+  .color(0.2,0,1)
+)
+.scale(1, () => window.innerHeight / window.innerWidth)
+.out()
 
-    .out(o0)
 
 }
 function bg01(){
@@ -32,5 +57,7 @@ function bg01(){
     .scale(1, () => window.innerHeight / window.innerWidth)
     .out(o0)
 }
+function bg03(){
+solid(0.2,0.6,0.9).layer(osc(31.4,0).thresh(0.7).luma().modulate(osc(4,1).rotate(1),0.05).color(0,0,0)).layer(osc(31.4,0).thresh(0.7).luma().modulate(osc(4,1).rotate(1),0.1)).out()
+}
 
-// create functions to use with buttons
